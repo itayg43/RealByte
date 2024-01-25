4ab47603f43297806a73401646fbf35f21209fab
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-01-25 22:50:55 +0200

    edit: image selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
+const apiClient = axios.create({
+  baseURL: "",
+});
+
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [selectedImageUrl, setSelectedImageUrl] = useState<string | null>(null);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files === null) return;
+
+    const file = e.target.files[0];
+    setSelectedImage(file);
+    setSelectedImageUrl(URL.createObjectURL(file));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (selectedImage === null) return;
+
+    const formData = new FormData();
+    formData.append("file", selectedImage);
+
+    await apiClient.post("", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="content-container-sm">
+        <h1 className="text-xxl text-center">Real Estate</h1>
+
+        {selectedImageUrl && (
+          // eslint-disable-next-line jsx-a11y/img-redundant-alt
+          <img src={selectedImageUrl} alt="selected image" />
+        )}
+
+        <form className="form-container" onSubmit={handleSubmit}>
+          <fieldset>
+            <label htmlFor="image-input" className="image-input">
+              {selectedImage ? "Change Image" : "Select Image"}
+            </label>
+
+            <input
+              id="image-input"
+              type="file"
+              accept="image/*"
+              onChange={handleInputChange}
+            />
+          </fieldset>
+
+          <button
+            className="button"
+            type="submit"
+            disabled={selectedImage === null}
+          >
+            Upload
+          </button>
+        </form>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
