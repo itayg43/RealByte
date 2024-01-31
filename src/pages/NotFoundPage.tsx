53644259009d5f25ebdce909488ca359fd897624
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-02-01 01:17:55 +0200

    edit: change to h2

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,5 @@
 const NotFoundPage = () => {
-  return <h1 className="text-xl text-center">404: Page Not Found</h1>;
+  return <h2 className="text-xl text-center">404: Page Not Found</h2>;
 };
 
 export default NotFoundPage;
