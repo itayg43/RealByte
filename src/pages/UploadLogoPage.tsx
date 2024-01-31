53af9531b2bb959bd2eb72efc8ac5e25996e89cb
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-02-01 01:49:18 +0200

    edit: rename value

--- src/pages/UploadLogoPage.tsx
+++ src/pages/UploadLogoPage.tsx
@@ -7,7 +7,7 @@ import UploadLogoForm, {
   type UploadLogoFormInputs,
 } from "../components/forms/UploadLogoForm";
 
-const REDIRECT_TIME_OUT_IN_MILLIS = 1500;
+const REDIRECT_DELAY_IN_MILLIS = 1500;
 
 const UploadLogoPage = () => {
   const { userPhoneNumber } = useParams();
@@ -20,7 +20,7 @@ const UploadLogoPage = () => {
     try {
       setUploadStatus("loading");
       await apiService.uploadLogo(userPhoneNumber, inputs.file[0]);
-      setTimeout(redirectToWhatsapp, REDIRECT_TIME_OUT_IN_MILLIS);
+      setTimeout(redirectToWhatsapp, REDIRECT_DELAY_IN_MILLIS);
       setUploadStatus("succeeded");
     } catch (error: any) {
       setUploadStatus("failed");
